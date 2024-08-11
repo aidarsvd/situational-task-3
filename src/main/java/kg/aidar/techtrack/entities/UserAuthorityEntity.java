ef59162fbcd9b45ae31a7a95457783af12637704
Aidar <achakiev2003@gmail.com>
2024-08-11 15:18:44 +0600

    create authority table

---
+++ src/main/java/kg/aidar/techtrack/entities/UserAuthorityEntity.java
@@ -0,0 +1,35 @@
+package kg.aidar.techtrack.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tech_track_authorities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserAuthorityEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    @Column(columnDefinition = "TEXT")
+    String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tech_track_users_authorities",
+            joinColumns = @JoinColumn(name = "authority_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    List<UserEntity> users;
+
+}
