07b2176aa1418b166bffd5685cbfed809ad402f2
Aidar <achakiev2003@gmail.com>
2024-08-11 15:22:56 +0600

    create authority configs

--- src/main/java/kg/aidar/techtrack/models/AppUserDetails.java
+++ src/main/java/kg/aidar/techtrack/models/AppUserDetails.java
@@ -6,6 +6,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,28 +26,25 @@ public class AppUserDetails implements UserDetails {
 
     String password;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
+    List<AuthorityModel> authorities;
+
+    boolean isEnabled;
+
+    LocalDateTime createdAt;
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return this.isEnabled;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return this.isEnabled;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return this.isEnabled;
     }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
---
+++ src/main/java/kg/aidar/techtrack/models/AuthorityModel.java
@@ -0,0 +1,19 @@
+package kg.aidar.techtrack.models;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthorityModel implements GrantedAuthority {
+
+    String authority;
+    String description;
+
+}
